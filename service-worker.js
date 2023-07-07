255a56de63f40f1bdc7debb7e65682e1b9729dbd
Yadu-M <ymadhu@ryerson.ca>
2023-07-07 13:16:41 -0400

    added cleanup function

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,13 @@
  */
 function cleanUpText(text) {
   // Remove new lines
+  
   text = text.replace(/\n/g, '');
 
   // Remove nonsensical characters
   text = text.replace(/[^\w\s.,!?]/g, '');
+
+  text = text.slice(4);
 
   return text;    
 }
@@ -21,26 +24,19 @@ chrome.runtime.onInstalled.addListener(({reason, previousVersion}) => {
   }
 });
 
-async function test(text){
-  importScripts("module.js");
-
-  const response = cleanUpText(text);
-
-  return response;
-}
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   // Handle the message received from the content script
-  console.log(message);
-
-  // Clean up the text
-  console.log(typeof(message));
-  // text = cleanUpText(message);
-  // console.log(text);
-
-  // Send a response back to the content script
-  sendResponse(true);
-
+  var text = '';
+  try {
+    text = JSON.stringify(message);
+    text = cleanUpText(text);
+  } catch (error) {
+    console.log(error);
+    sendResponse(false);
+  }
+  console.log(text);
+  sendResponse(true);  
+  
   // Explicitly return true to indicate that sendResponse will be called asynchronously
   return true;
 });
